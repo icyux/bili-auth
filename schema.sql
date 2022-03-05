1194cc5f82b7f3a4fa65db084067f7bbb474ae01
vapehacker <me@icyu.me>
2022-03-05 21:14:44 +0800

    chore: update database schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,29 @@
 CREATE TABLE "app" (
 	"cid"	TEXT NOT NULL UNIQUE,
+	"sec"	TEXT NOT NULL,
 	"name"	TEXT,
 	"url"	TEXT,
-	"sec"	TEXT NOT NULL,
+	"desc"	TEXT,
+	"icon"	TEXT,
 	PRIMARY KEY("cid")
-)
+);
+
+CREATE TABLE "verify" (
+	"vid"		INTEGER NOT NULL UNIQUE,
+	"clgCode"	TEXT NOT NULL UNIQUE,
+	"create"	INTEGER NOT NULL,
+	"expire"	INTEGER NOT NULL,
+	"uid"		INTEGER,
+	PRIMARY KEY("vid")
+);
+
+CREATE TABLE "session" (
+	"sid"		INTEGER NOT NULL UNIQUE,
+	"vid"		INTEGER NOT NULL,
+	"uid"		INTEGER NOT NULL,
+	"cid"		INTEGER NOT NULL,
+	"create"	INTEGER NOT NULL,
+	"accCode"	TEXT,
+	"token"		TEXT,
+	PRIMARY KEY("sid" AUTOINCREMENT)
+);
